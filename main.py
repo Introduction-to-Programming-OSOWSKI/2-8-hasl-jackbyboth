aa49f1740c6ad74ba1b72d55342e1c94d36af676
jackbyboth23 <817581@mystma.org>
2021-10-05 15:09:48 -0500

    2

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #WRITE YOUR CODE IN THIS FILE
+def hasL(x):
+
+
+    for i in range(0, len(x)):
+        
+        if x[i] == "l":
+            return True
+        
+        
+    return False
+print(hasL("abama"))
